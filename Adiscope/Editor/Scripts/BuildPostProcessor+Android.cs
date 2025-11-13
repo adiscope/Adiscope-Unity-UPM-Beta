5d40599eb8b34eab9e194e19807a7bb79fedfcb1
kyungboshim <kyungbo.shim@neowiz.com>
2025-11-13 09:49:14 +0900

    5.0.1 배포

--- Adiscope/Editor/Scripts/BuildPostProcessor+Android.cs
+++ Adiscope/Editor/Scripts/BuildPostProcessor+Android.cs
@@ -508,10 +508,10 @@ android {{
 
         // private const string ADISCOPE_FILE_PATH = "https://github.com/adiscope/Adiscope-Android-Sample/releases/download/";
         private const string ADISCOPE_FILE_PATH = "https://github.com/adiscope/Adiscope-Unity-UPM-Beta/releases/download/";
-        private const string ADMOB_FILE_PATH        = ADISCOPE_FILE_PATH + "5.0.0/";
+        private const string ADMOB_FILE_PATH        = ADISCOPE_FILE_PATH + "5.0.1/";
         private const string CHARTBOOST_FILE_PATH   = ADISCOPE_FILE_PATH + "5.0.0/";
-        private const string MAX_FILE_PATH          = ADISCOPE_FILE_PATH + "5.0.0/";
-        private const string PANGLE_FILE_PATH       = ADISCOPE_FILE_PATH + "5.0.0/";
+        private const string MAX_FILE_PATH          = ADISCOPE_FILE_PATH + "5.0.1/";
+        private const string PANGLE_FILE_PATH       = ADISCOPE_FILE_PATH + "5.0.1/";
         private const string VUNGLE_FILE_PATH       = ADISCOPE_FILE_PATH + "5.0.0/";
 
         public static string GetFileName(this AdiscopeFrameworkAndroidType type)
